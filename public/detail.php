9391bd7fe056e754b0a17c15ce5a49a119ce41b3
nixpluvia <whbear12@gmail.com>
2020-06-26 18:42:01 +0900

    데이터베이스 리스트 추가, 글페이지 추가

--- public/detail.php
+++ public/detail.php
@@ -7,20 +7,15 @@ include "../part/head_body.php";
 ?>
 <?php
 $conn = mysqli_connect('localhost','sbsst','sbs123414','blog',3306);
+$id = $_GET['id'];
 $sql = "
 SELECT *
 FROM article
-LIMIT 100
+WHERE id = {$id}
 ";
 $rs = mysqli_query($conn, $sql);
-$articles = [];
-
-while( $article = mysqli_fetch_assoc($rs)) {
-    $articles[] = $article;
-}
-
+$row = mysqli_fetch_assoc($rs);
 ?>
-
 <!--하이라이트 라이브러리-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
 <!--하이라이트 언어-->
@@ -42,38 +37,35 @@ while( $article = mysqli_fetch_assoc($rs)) {
 <!--토스트 하이라이트 플러그인-->
 <script src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js"></script>
 
-<article class="article-box con flex flex-wrap">
-    <?php foreach( $articles as $article ) { ?>
-        <div id="origin1" style="display:none;">
-            <?=$article['body']?>
-        </div>
-        <div class="article-list flex">
-            <div class="article-content">
-                <a href="/detail.php?id=<?=$article['id']?>" class="article-title">
-                    <h2><?=$article['title']?></h2>
-                </a>
-                <div class="article-body" id="viewer1">
-                </div>
-                <div class="article-info flex">
-                    <div><?=$article['regDate']?></div>
-                    <div><?=$article['updateDate']?></div>
-                </div>
-                <div class="article-tag-bar flex">
-                    <div class="article-tag flex flex-ai-c">#태그</div>
-                </div>
-            </div>
-            <a href="#" class="article-img" style="background-image: url(https://cdn.pixabay.com/photo/2020/01/31/07/26/japan-4807317_960_720.jpg)"></a>
-        </div>
-    <?php } ?>
-</article>
+<div class="con">
+    <a href="#" onclick="history.back();">[뒤로가기]</a>
+    <a href="/itPrograming.php">[리스트]</a>
+</div>
+
+<h1 class="con">제목 : <?=$row['title']?></h1>
+<div class="con">
+    등록날짜 : <?=$row['regDate']?>
+</div>
+<div class="con">
+    수정날짜 : <?=$row['updateDate']?>
+</div>
+<div class="con">
+    작성자 : 장희성
+</div>
+<div class="con" style="display:none;" id="origin1">
+<?=$row['body']?>
+</div>
+<div class="con" id="viewer1">
+</div>
 
 <script>
 var editor1__initialValue = $('#origin1').html();
 var editor1 = new toastui.Editor({
-    el : document.querySelector('#viewer1'),
-    initialValue: editor1__initialValue,
-    viewer: true,
-    plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+  el: document.querySelector('#viewer1'),
+  height: '600px',
+  initialValue: editor1__initialValue,
+  viewer:true,
+  plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
 });
 </script>
 
