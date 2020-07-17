b88b06e9f656a1cf82a2f5bb91428e7a8bd4c54c
nixpluvia <whbear12@gmail.com>
2020-07-17 17:37:41 +0900

    포트폴리오 아트워크 추가

--- public/pf/index.php
+++ public/pf/index.php
@@ -8,10 +8,19 @@
 <body>
     <ul>
         <li>
-            <a href="/pf/cleaderm">클리덤 닥터락토</a>
+            <a href="https://cleaderm.co.kr/" target="_blank">클리덤 닥터락토 원본 사이트</a>
         </li>
         <li>
-            <a href="/pf/heimish">헤이미쉬 공식 온라인몰</a>
+            <a href="/pf/cleaderm" target="_blank">클리덤 닥터락토</a>
+        </li>
+        <li>
+            <a href="http://www.eheimish.com/" target="_blank">헤이미쉬 원본 사이트</a>
+        </li>
+        <li>
+            <a href="/pf/heimish" target="_blank">헤이미쉬 공식 온라인몰</a>
+        </li>
+        <li>
+            <a href="https://nixpluvia.github.io/img1/pf/artworks/1/1.png" target="_blank">아트워크1</a>
         </li>
     </ul>
 </body>
