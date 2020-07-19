9d8906db9a17f621a098d7b03cb27effd30ab6e8
nixpluvia <whbear12@gmail.com>
2020-07-20 03:08:25 +0900

    슬라임 슬라이드 수정

--- public/portfolio.php
+++ public/portfolio.php
@@ -2,6 +2,7 @@
 include "../part/head_head.php";
 ?>
 <link rel="stylesheet" href="/resource/portfolio.css">
+<script src="/resource/portfolio.js"></script>
 <?php
 include "../part/head_body.php";
 ?>
@@ -10,12 +11,22 @@ include "../part/head_body.php";
     <div class="ani-img">
         <div class="img-head img-box">
             <img src="https://nixpluvia.github.io/img1/blog/site/cutslime-head.svg" alt="">
+            <img class="slime-eye left" src="https://nixpluvia.github.io/img1/blog/site/eye-ani-big.gif" alt="">
+            <img class="slime-eye right" src="https://nixpluvia.github.io/img1/blog/site/eye-ani-big.gif" alt="">
         </div>
-        <div class="portfolio-slider">
-            <i class="teeth-left"></i>
-            <i class="teeth-right"></i>
-            <div class="portfolio-slides">
-                <div class="slide"></div>
+        <div class="slider portfolio-slider">
+            <div class="btn-slide">
+                <i class="teeth-left"></i>
+                <i class="teeth-right"></i>
+            </div>
+            <div id="" class="slides portfolio-slides">
+                <a class="slide active" href="#" style="background-color: red;"></a>
+                <a class="slide right-set" href="#" style="background-color: blue;"></a>
+                <a class="slide" href="#" style="background-color: green;"></a>
+                <a class="slide" href="#" style="background-color: yellow;"></a>
+                <a class="slide" href="#" style="background-color: purple;"></a>
+                <a class="slide" href="#" style="background-color: indigo;"></a>
+                <a class="slide left-set" href="#" style="background-color: orange;"></a>
             </div>
         </div>
         <div class="img-foot img-box">
