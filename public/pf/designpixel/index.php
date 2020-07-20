1e1d1abf2298c709083df590e93e8a0fd796ce3e
nixpluvia <whbear12@gmail.com>
2020-07-21 01:59:24 +0900

    디자인픽셀 모작 사이트 슬라이드 작업

--- public/pf/designpixel/index.php
+++ public/pf/designpixel/index.php
@@ -10,6 +10,47 @@
 </head>
 <body>
     <div class="layout">
+        <a class="logo img-box" href="#">
+            <img src="https://nixpluvia.github.io/img1/pf/designpixel/main_logo_white.png" alt="">
+        </a>
+        <div class="slider main-slide-box">
+            <div class="slides">
+                <div class="slide active">
+                    <div class="slide-bg"></div>
+                    <div class="text-box">
+                        <svg class="text-box-top">
+                            <text class="top-text" x="50%" y="50%" >BEYOND TECH.</text>
+                        </svg>
+                        <svg class="text-box-bottom">
+                            <text class="bottom-text">SOULBRAIN</text>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            <div class="btn-slide">
+                <a class="btn-top btn" href="#">
+                    <img src="https://nixpluvia.github.io/img1/pf/designpixel/i_slide_lt.svg" alt="">
+                </a>
+                <div class="slide-num">
+                    <div class="num-now num">01</div>
+                    <div class="line"></div>
+                    <div class="num-total num">10</div>
+                </div>
+                <a class="btn-bottom btn" href="#">
+                    <img src="https://nixpluvia.github.io/img1/pf/designpixel/i_slide_gt.svg" alt="">
+                </a>
+            </div>
+        </div>
+        
+        <div class="footer">
+            <div class="com-ad">
+                서울시 서초구 서초동 1440-2 픽셀빌딩 4~5층
+            </div>
+            <div class="copyright">
+                (C) 2020 DESIGNPIXEL.CO.KR ALL RIGHTS RESERVED T.02.587.1152
+            </div>
+        </div>
     </div>
 </body>
 </html>
