27e43910db814c48a58c09993f1ac380aff21f8e
nixpluvia <whbear12@gmail.com>
2020-07-20 18:47:36 +0900

    디자인픽셀 모작 생성

---
+++ public/pf/designpixel/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+    <title>디자인 픽셀 | DESIGN PIXEL</title>
+</head>
+<body>
+    <div class="layout">
+    </div>
+</body>
+</html>
