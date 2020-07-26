32ebc41f3c80d5b5467b9859708ec827b8ae7b35
nixpluvia <whbear12@gmail.com>
2020-07-26 19:37:30 +0900

    블로그 포트폴리오 업데이트

--- public/portfolio.php
+++ public/portfolio.php
@@ -20,13 +20,9 @@ include "../part/head_body.php";
                 <i class="teeth-right"></i>
             </div>
             <div id="" class="slides portfolio-slides">
-                <a class="slide active" href="#" style="background-color: red;"></a>
-                <a class="slide right-set" href="#" style="background-color: blue;"></a>
-                <a class="slide" href="#" style="background-color: green;"></a>
-                <a class="slide" href="#" style="background-color: yellow;"></a>
-                <a class="slide" href="#" style="background-color: purple;"></a>
-                <a class="slide" href="#" style="background-color: indigo;"></a>
-                <a class="slide left-set" href="#" style="background-color: orange;"></a>
+                <a class="slide active" href="/pf/designpixel/index.php" target="_blank" style="background-image: url(https://nixpluvia.github.io/img1/blog/site/designpixel_pf.png)"></a>
+                <a class="slide right-set" href="/pf/heimish/index.php" target="_blank" style="background-image: url(https://nixpluvia.github.io/img1/blog/site/heimish_pf.png"></a>
+                <a class="slide left-set" href="/pf/cleaderm/index.php" target="_blank" style="background-image: url(https://nixpluvia.github.io/img1/blog/site/cleaderm_pf.png"></a>
             </div>
         </div>
         <div class="img-foot img-box">
@@ -34,7 +30,44 @@ include "../part/head_body.php";
         </div>
     </div>
 </div>
-
+<nav>
+        <ul>
+            <li>
+                <a href="/pf/cleaderm" target="_blank">클리덤 닥터락토</a>
+                <ul>
+                    <li>
+                        <a href="https://cleaderm.co.kr/" target="_blank">클리덤 닥터락토 원본 사이트</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="/pf/heimish" target="_blank">헤이미쉬 공식 온라인몰</a>
+                <ul>
+                    <li>
+                        <a href="http://www.eheimish.com/" target="_blank">헤이미쉬 원본 사이트</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="/pf/designpixel" target="_blank">디자인 픽셀 모작</a>
+                <ul>
+                    <li>
+                        <a href="https://designpixel.co.kr/" target="_blank">디자인 픽셀 원본 사이트</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <h1>디자인</h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="https://nixpluvia.github.io/img1/pf/artworks/1/1.png" target="_blank">아트워크1</a>
+                <ul><li>창작여부 : 모작</li></ul>
+                <ul><li>출처: <a href="https://www.mangoboard.net/MangoTemplateAll.do">망고보드</a></li></ul>
+            </li>
+        </ul>
+    </nav>
 <?php
 include "../part/foot.php";
 ?>
