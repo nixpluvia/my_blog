441604b7a2f856dbb425b2cec334c25896026437
nixpluvia <whbear12@gmail.com>
2020-08-25 23:56:41 +0900

    포트폴리오 페이지

---
+++ public/pf/portfolio/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="/pf/portfolio/index.css">
+    <script src="/pf/portfolio/index.js"></script>
+    <title>portfolio</title>
+</head>
+<body>
+<div class="back"></div>
+    
+</body>
+</html>
